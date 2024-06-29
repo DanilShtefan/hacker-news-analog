4a4d287a335f5bc7714e3aeb920de16f2b3ccacf
DanilShtefan <danilshtefan.66@gmail.com>
2024-06-30 00:21:57 +0300

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/hacker-news-analog',
+    base: '/hacker-news-analog/',
     plugins: [react()],
 });
